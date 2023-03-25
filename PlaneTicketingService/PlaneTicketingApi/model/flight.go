d2eec8321e65f75dd4ab56046bf793f7677ae84a
Halid Pasanovic <halidpasanovic1000@gmail.com>
2023-03-25 14:40:42 +0100

    small fix

--- PlaneTicketingService/PlaneTicketingApi/model/flight.go
+++ PlaneTicketingService/PlaneTicketingApi/model/flight.go
@@ -8,10 +8,10 @@ import (
 
 type Flight struct {
 	Id                  primitive.ObjectID `bson:"_id" json:"id"`
-	StartDateTimeUTC    time.Time          `bson:"_id" json:"id"`
-	EndDateTimeUTC      time.Time          `bson:"_id" json:"id"`
-	DepartureLocation   string             `bson:"_id" json:"id"`
-	DestinationLocation string             `bson:"_id" json:"id"`
-	Price               float64            `bson:"_id" json:"id"`
-	AvailableTickets    []Ticket           `bson:"_id" json:"id"`
+	StartDateTimeUTC    time.Time          `bson:"startDateTimeUTC" json:"startDateTimeUTC"`
+	EndDateTimeUTC      time.Time          `bson:"endDateTimeUTC" json:"endDateTimeUTC"`
+	DepartureLocation   string             `bson:"departureLocation " json:"departureLocation"`
+	DestinationLocation string             `bson:"destinationLocation" json:"destinationLocation"`
+	Price               float64            `bson:"price" json:"price"`
+	AvailableTickets    []Ticket           `bson:"availableTickets" json:"availableTickets"`
 }
--- PlaneTicketingService/PlaneTicketingApi/model/ticket.go
+++ PlaneTicketingService/PlaneTicketingApi/model/ticket.go
@@ -3,6 +3,6 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Ticket struct {
-	Id    primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Id    primitive.ObjectID `bson:"_id" json:"id"`
 	Price float64            `bson:"price" json:"price"`
 }
